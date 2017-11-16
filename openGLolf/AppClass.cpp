6917f7f5524642f38def3d3e63b1725bef1b36c1
MAIN\cdb6474 <cdb6474@g.rit.edu>
2017-11-16 10:39:32 -0500

    Updated a lot of the Repo, dealing with implementing the physics code for everything, hope to test out a swing mechanic soon

---
+++ openGLolf/AppClass.cpp
@@ -0,0 +1,59 @@
+#include "AppClass.h"
+using namespace Simplex;
+void Application::InitVariables(void)
+{
+	//Set the position and target of the camera
+	m_pCameraMngr->SetPositionTargetAndUp(
+		vector3(0.0f, 0.0f, 10.0f), //Position
+		vector3(0.0f, 0.0f, 9.0f),	//Target
+		AXIS_Y);					//Up
+
+	m_pLightMngr->SetPosition(vector3(0.0f, 3.0f, 13.0f), 1); //set the position of first light (0 is reserved for ambient light)
+
+	m_pEntityMngr->AddEntity("Minecraft\\Steve.obj", "Ball");
+	m_pEntityMngr->UsePhysicsSolver();
+
+	m_pEntityMngr->Update();
+}
+void Application::Update(void)
+{
+	//Update the system so it knows how much time has passed since the last call
+	m_pSystem->Update();
+
+	//Is the ArcBall active?
+	ArcBall();
+
+	//Is the first person camera active?
+	CameraRotation();
+	
+	//Update Entity Manager
+	m_pEntityMngr->Update();
+
+	//Add objects to render list
+	m_pEntityMngr->AddEntityToRenderList(-1, true);
+}
+void Application::Display(void)
+{
+	// Clear the screen
+	ClearScreen();
+	
+	// draw a skybox
+	m_pMeshMngr->AddSkyboxToRenderList();
+	
+	//render list call
+	m_uRenderCallCount = m_pMeshMngr->Render();
+
+	//clear the render list
+	m_pMeshMngr->ClearRenderList();
+	
+	//draw gui,
+	DrawGUI();
+	
+	//end the current frame (internally swaps the front and back buffers)
+	m_pWindow->display();
+}
+void Application::Release(void)
+{
+	//release GUI
+	ShutdownGUI();
+}
